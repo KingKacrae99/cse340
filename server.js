8f06eca1815f8fc17d8dfff48a6713b47cf19471
kingkacrae99 <kacrae9@gmail.com>
2026-06-08 16:15:12 +0100

    Database Updated

--- server.js
+++ server.js
@@ -108,14 +108,14 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
+  //let nav = await utilities.getNav()
   //let message
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   if (err.status == 404){message = err.message} else{message='Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
-    nav
+    //nav
   })
 })
 
